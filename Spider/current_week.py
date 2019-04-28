bb115c1e2945221d5c417aa8f59bc85fcce92cb3
qiuyue <qiuyue9971@126.com>
2019-04-28 21:54:10 +0800

    formated according to pep8

--- Spider/current_week.py
+++ Spider/current_week.py
@@ -12,6 +12,7 @@ logging.basicConfig(level=logging.ERROR,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+
 def __get_headers():
     headers = [
         {
